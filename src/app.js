bfceafa28c70c47c0e9fe1bcb38d3f65f4bc0c14
Dirk Podolak <dirk.podolak@gmail.com>
2022-07-17 14:20:11 +0200

    Zwischencommit: soll tabelle.mannschaften ein Array oder ein Dictionary sein?

---
+++ src/app.js
@@ -0,0 +1,200 @@
+const mannschaften = {
+  0: {
+    name: "Bayern München"
+  },
+  1: {
+    name: "Borussia Dortmund"
+  },
+  2: {
+    name: "Bayer 04 Leverkusen"
+  },
+  3: {
+    name: "RB Leipzig"
+  },
+  4: {
+    name: "Eintracht Frankfurt"
+  }
+}
+const anzahlMannschaften = Object.keys(mannschaften).length
+
+function createTestspiele() {
+  const saisons = {
+    2022: {
+      saison: "2022/2023",
+      spieltage: {}
+    }
+  }
+
+  const saisonIndex = 2022
+  const saison = saisons[saisonIndex]
+
+  let datum = new Date("2022-08-06T13:30:00Z")
+
+  for (let spieltagIndex = 0; spieltagIndex < 5; spieltagIndex++) {
+    const spieltag = {
+      spiele: {}
+    }
+    saison.spieltage[spieltagIndex] = spieltag
+
+    for (let spielIndex = 0; spielIndex < 4; spielIndex++) {
+      const indexHeim = Math.floor(Math.random() * anzahlMannschaften)
+      let indexGast = Math.floor(Math.random() * anzahlMannschaften)
+      while (indexGast === indexHeim) {
+        indexGast = Math.floor(Math.random() * anzahlMannschaften)
+      }
+
+      spieltag.spiele[spielIndex] = {
+        datum: datum,
+        heim: indexHeim,
+        gast: indexGast,
+        toreHeim: Math.floor(Math.random() * 4),
+        toreGast: Math.floor(Math.random() * 4)
+      }
+    }
+
+    datum.setDate(datum.getDate() + 7)
+  }
+
+  return saisons
+}
+
+function addSpieltagToTabelle(tabelle, spieltag) {
+  for (const id of Object.keys(spieltag.spiele)) {
+    const spiel = spieltag.spiele[id]
+
+    const mannschaftHeim = tabelle.mannschaften[spiel.heim]
+    mannschaftHeim.spiele += 1
+    mannschaftHeim.tore += spiel.toreHeim
+    mannschaftHeim.gegentore += spiel.toreGast
+    if (spiel.toreHeim > spiel.toreGast) {
+      mannschaftHeim.punkte += 3
+    } else if (spiel.toreHeim === spiel.toreGast) {
+      mannschaftHeim.punkte += 1
+    }
+    
+    const mannschaftGast = tabelle.mannschaften[spiel.gast]
+    mannschaftGast.spiele += 1
+    mannschaftGast.tore += spiel.toreGast
+    mannschaftGast.gegentore += spiel.toreHeim
+    if (spiel.toreGast > spiel.toreHeim) {
+      mannschaftGast.punkte += 3
+    } else if (spiel.toreGast === spiel.toreHeim) {
+      mannschaftGast.punkte += 1
+    }
+
+  }
+}
+
+const dummyTabelle = {
+  spieltag: 3,
+  mannschaften: [
+    {
+      id: 0,
+      tore: 97,
+      gegentore: 37,
+      punkte: 77
+    },{
+      id: 1,
+      tore: 85,
+      gegentore: 52,
+      punkte: 69
+    }
+  ]
+}
+
+const dummyTabelle2 = {
+  spieltag: 3,
+  mannschaften: {
+    0: {
+      id: 0,
+      spiele: 34,
+      tore: 97,
+      gegentore: 37,
+      punkte: 77
+    },
+    1: {
+      id: 1,
+      spiele: 34,
+      tore: 85,
+      gegentore: 52,
+      punkte: 69
+    }
+  }
+}
+
+const dummyTabelle3 = {
+  spieltag: 3,
+  mannschaften: {
+    0: {
+      id: 0,
+      spiele: 34,
+      tore: 97,
+      gegentore: 37,
+      punkte: 77
+    },
+    1: {
+      id: 1,
+      spiele: 34,
+      tore: 85,
+      gegentore: 52,
+      punkte: 69
+    }
+  }
+}
+
+function mannschaftComparator(mannschaft0, mannschaft1) {
+  if (mannschaft0.punkte > mannschaft1.punkte) {
+    return 1
+  } else if (mannschaft0.punkte < mannschaft1.punkte) {
+    return -1
+  } else {
+    const tordifferenz0 = mannschaft0.tore - mannschaft0.gegentore
+    const tordifferenz1 = mannschaft1.tore - mannschaft1.gegentore
+
+    if (tordifferenz0 > tordifferenz1) {
+      return 1
+    } else if (tordifferenz0 < tordifferenz1) {
+      return -1
+    } else {
+      if (mannschaft0.tore > mannschaft1.tore) {
+        return 1
+      } else if (mannschaft0.tore < mannschaft1.tore) {
+        return -1
+      } else {
+        return 0
+      }
+    }
+  }
+}
+
+function fillTabelle(spieltage, spieltagnummer) {
+  const tabelle = {
+    spieltag: 0,
+    mannschaften: {}
+  }
+  for (const id of Object.keys(mannschaften)) {
+    tabelle.mannschaften[id] = {
+      id: id,
+      spiele: 0,
+      tore: 0,
+      gegentore: 0,
+      punkte: 0
+    }
+  }
+  for (let i = 0; i <= spieltagnummer; i++) {
+    addSpieltagToTabelle(tabelle, spieltage[i])
+  }
+
+console.log(tabelle)
+
+  const sortedMannschaften = tabelle.mannschaften.sort(mannschaftComparator)
+console.log(tabelle)
+
+  const nameElement = document.querySelector('.tabelle [data-index="1"]')
+  //console.log(nameElement)
+}
+
+const saisons = createTestspiele()
+//console.log(saisons)
+
+fillTabelle(saisons[2022].spieltage, 2)
