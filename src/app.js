897889c79e2f7d55b87a606d6a81b16810e8e304
Dirk Podolak <dirk.podolak@gmail.com>
2022-07-27 16:25:52 +0200

    alte Dateien entfernt neue Tasks aufgeschrieben Darstellung des Spieltags korrigiert

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-const sleep = ms => new Promise(r => setTimeout(r, ms))
-
 const mannschaften = {
   0: {
     name: "Arminia Bielefeld"
@@ -55,24 +53,28 @@ const mannschaften = {
   17: {
     name: "Hamburger SV"
   }
-
-
-
-
-  
 }
+
 const anzahlMannschaften = Object.keys(mannschaften).length
 const anzahlSpieleProSpieltag = anzahlMannschaften / 2
 const anzahlSpieltage = (anzahlMannschaften - 1) * 2
 let aktuelleSaison = 2022
 let aktuellerSpieltag = -1
 
-const spielplan = {
-
+function formatDatum(datum) {
+  if (datum instanceof Date) {
+    return datum.toLocaleDateString("DE-de", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric"
+    })
+  } else {
+    return formatDatum(new Date(datum))
+  }
 }
 
 function createTestspiele() {
-  const saisons = {
+  const saisonList = {
     2022: {
       saison: "2022/2023",
       spieltage: {}
@@ -80,17 +82,25 @@ function createTestspiele() {
   }
 
   const saisonIndex = 2022
-  const saison = saisons[saisonIndex]
+  const saison = saisonList[saisonIndex]
 
   let datum = new Date("2022-08-06T13:30:00Z")
 
-  for (let spieltagIndex = 0; spieltagIndex < anzahlSpieltage; spieltagIndex++) {
+  for (
+    let spieltagIndex = 0;
+    spieltagIndex < anzahlSpieltage;
+    spieltagIndex++
+  ) {
     const spieltag = {
       spiele: {}
     }
     saison.spieltage[spieltagIndex] = spieltag
 
-    for (let spielIndex = 0; spielIndex < anzahlSpieleProSpieltag; spielIndex++) {
+    for (
+      let spielIndex = 0;
+      spielIndex < anzahlSpieleProSpieltag;
+      spielIndex++
+    ) {
       const indexHeim = Math.floor(Math.random() * anzahlMannschaften)
       let indexGast = Math.floor(Math.random() * anzahlMannschaften)
       while (indexGast === indexHeim) {
@@ -98,7 +108,7 @@ function createTestspiele() {
       }
 
       spieltag.spiele[spielIndex] = {
-        datum: datum,
+        datum: datum.toISOString(),
         heim: indexHeim,
         gast: indexGast,
         toreHeim: Math.floor(Math.random() * 4),
@@ -109,10 +119,10 @@ function createTestspiele() {
     datum.setDate(datum.getDate() + 7)
   }
 
-  return saisons
+  return saisonList
 }
 
-function addSpieltagToTabelle(tabelle, spieltag) {
+function addSpieltagToTabelle(tabelle, spieltag) {  
   for (const id of Object.keys(spieltag.spiele)) {
     const spiel = spieltag.spiele[id]
 
@@ -125,7 +135,7 @@ function addSpieltagToTabelle(tabelle, spieltag) {
     } else if (spiel.toreHeim === spiel.toreGast) {
       mannschaftHeim.punkte += 1
     }
-    
+
     const mannschaftGast = tabelle.mannschaften[spiel.gast]
     mannschaftGast.spiele += 1
     mannschaftGast.tore += spiel.toreGast
@@ -135,64 +145,7 @@ function addSpieltagToTabelle(tabelle, spieltag) {
     } else if (spiel.toreGast === spiel.toreHeim) {
       mannschaftGast.punkte += 1
     }
-
   }
-}
-
-const dummyTabelle = {
-  spieltag: 3,
-  mannschaften: [
-    {
-      id: 0,
-      tore: 97,
-      gegentore: 37,
-      punkte: 77
-    },{
-      id: 1,
-      tore: 85,
-      gegentore: 52,
-      punkte: 69
-    }
-  ]
-}
-
-const dummyTabelle2 = {
-  spieltag: 3,
-  mannschaften: {
-    0: {
-      id: 0,
-      spiele: 34,
-      tore: 97,
-      gegentore: 37,
-      punkte: 77
-    },
-    1: {
-      id: 1,
-      spiele: 34,
-      tore: 85,
-      gegentore: 52,
-      punkte: 69
-    }
-  }
-}
-
-const dummyTabelle3 = {
-  spieltag: 3,
-  mannschaften: [
-    {
-      id: 0,
-      spiele: 34,
-      tore: 97,
-      gegentore: 37,
-      punkte: 77
-    },{
-      id: 1,
-      spiele: 34,
-      tore: 85,
-      gegentore: 52,
-      punkte: 69
-    }
-  ]
 }
 
 function mannschaftComparator(mannschaft0, mannschaft1) {
@@ -222,13 +175,12 @@ function mannschaftComparator(mannschaft0, mannschaft1) {
 
 function createCell(text) {
   const cellNode = document.createElement("cell")
-  //cellNode.id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
   const textNode = document.createTextNode(text)
   cellNode.appendChild(textNode)
   return cellNode
 }
 
-function fillTabelle2() {
+function fillTabelle() {
   const spieltage = saisons[aktuelleSaison].spieltage
 
   const tabelle = {
@@ -245,200 +197,120 @@ function fillTabelle2() {
     })
   }
 
-/*
-  const nameDaten2 = Array()
-  for (const mannschaft of tabelle.mannschaften) {
-    nameDaten2.push(mannschaften[mannschaft.id].name)
-  }
-  */
-
-
-
-  let platzDaten = [...Array(18).keys()].map(i => createCell((i + 1) + '.'))
+  let platzDaten = [...Array(18).keys()].map((i) => createCell(i + 1 + "."))
   let nameDaten
   let spieleDaten
   let toreDaten
   let punkteDaten
 
+  // Daten ermitteln als ...
   if (aktuellerSpieltag === -1) {
-    nameDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaften[mannschaft.id].name))
-    spieleDaten = Array(tabelle.mannschaften.length).fill(createCell("0"))
-    toreDaten = Array(tabelle.mannschaften.length).fill(createCell("0:0"))
-    punkteDaten = Array(tabelle.mannschaften.length).fill(createCell("0"))
+    // ... Initialbefüllung, wenn vor dem ersten Spieltag
+    nameDaten = Array.from(tabelle.mannschaften, (mannschaft) => createCell(mannschaften[mannschaft.id].name))
+    spieleDaten = Array.from({ length: tabelle.mannschaften.length }, () => createCell("0"))
+    toreDaten = Array.from({ length: tabelle.mannschaften.length }, () => createCell("0:0"))
+    punkteDaten = Array.from({ length: tabelle.mannschaften.length }, () => createCell("0"))
   } else {
+    // ... berechnetem aktuellen Tabellenstand, wenn an einem gegebenen Spieltag
+
+    // Hier wird jedesmal von Vorne begonnen, die Tabelle komplett neu aufzubauen. Man könnte auch nur
+    // beim Blättern (voriger/nächster Spieltag) jeweils einen Spieltag weg oder dazu nehmen, aber dann
+    // könnte man nicht zu einem beliebigen Spieltag springen (z.B. per deep link).
+    // Da das ganze praktisch keine Zeit kostet ("duration: 0ms") lasse ich das einfach so.
     for (let i = 0; i <= aktuellerSpieltag; i++) {
       addSpieltagToTabelle(tabelle, spieltage[i])
     }
-
     tabelle.mannschaften.sort(mannschaftComparator)
 
-    nameDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaften[mannschaft.id].name))
-    spieleDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaft.spiele))
-    toreDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaft.tore + ":" + mannschaft.gegentore))
-    punkteDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaft.punkte))
+    // Nach der Berechnung der Tabelle des aktuellen Spieltags kann man jetzt einfach die Daten rausmappen.
+    nameDaten = Array.from(tabelle.mannschaften, (mannschaft) => createCell(mannschaften[mannschaft.id].name))
+    spieleDaten = Array.from(tabelle.mannschaften, (mannschaft) => createCell(mannschaft.spiele))
+    toreDaten = Array.from(tabelle.mannschaften, (mannschaft) => createCell(mannschaft.tore + ":" + mannschaft.gegentore))
+    punkteDaten = Array.from(tabelle.mannschaften, (mannschaft) => createCell(mannschaft.punkte))
   }
 
-
-
-
-
+  // Überschrift davorsetzen
   platzDaten.unshift(createCell("Platz"))
   nameDaten.unshift(createCell("Name"))
   spieleDaten.unshift(createCell("Spiele"))
   toreDaten.unshift(createCell("Tore"))
   punkteDaten.unshift(createCell("Punkte"))
 
-  const platzColumnElement = document.querySelector('.tabelle [data-type="platz"]')
-  platzColumnElement.replaceChildren(...platzDaten)
-  
-  const nameColumnElement = document.querySelector('.tabelle [data-type="name"]')
-  nameColumnElement.replaceChildren(...nameDaten)
-
-  const spieleColumnElement = document.querySelector('.tabelle [data-type="spiele"]')
-  console.log(spieleColumnElement)
-  console.log(spieleDaten)
-  console.log(...spieleDaten)
-
-  
-  spieleColumnElement.replaceChildren(...spieleDaten)
-  console.log(spieleColumnElement)
-
-  const toreColumnElement = document.querySelector('.tabelle [data-type="tore"]')
-  toreColumnElement.replaceChildren(...toreDaten)
-
-  const punkteColumnElement = document.querySelector('.tabelle [data-type="punkte"]')
-  punkteColumnElement.replaceChildren(...punkteDaten)
-
-
-
-  
-}
-
-async function fillTabelle() {
-  const spieltage = saisons[aktuelleSaison].spieltage
-
-  const tabelle = {
-    spieltag: 0,
-    mannschaften: []
-  }
-  for (const id of Object.keys(mannschaften)) {
-    tabelle.mannschaften.push({
-      id: id,
-      spiele: 0,
-      tore: 0,
-      gegentore: 0,
-      punkte: 0
-    })
-  }
-
-
-
-
-
-
-
-  let platzDaten = [...Array(18).keys()].map(i => createCell((i + 1) + '.'))
-  let nameDaten
-  let spieleDaten
-  let toreDaten
-  let punkteDaten
-
-  
-
-  if (aktuellerSpieltag === -1) {
-    nameDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaften[mannschaft.id].name))
-    spieleDaten = Array.from({length: tabelle.mannschaften.length}, () => createCell('0'))
-    toreDaten = Array.from({length: tabelle.mannschaften.length}, () => createCell('0:0'))
-    punkteDaten = Array.from({length: tabelle.mannschaften.length}, () => createCell('0'))
-  } else {
-    for (let i = 0; i <= aktuellerSpieltag; i++) {
-      addSpieltagToTabelle(tabelle, spieltage[i])
-    }
-
-    tabelle.mannschaften.sort(mannschaftComparator)
-
-    nameDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaften[mannschaft.id].name))
-    spieleDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaft.spiele))
-    toreDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaft.tore + ":" + mannschaft.gegentore))
-    punkteDaten = Array.from(tabelle.mannschaften, mannschaft => createCell(mannschaft.punkte))
-  }
-
-  platzDaten.unshift(createCell("Platz"))
-  nameDaten.unshift(createCell("Name"))
-  spieleDaten.unshift(createCell("Spiele"))
-  toreDaten.unshift(createCell("Tore"))
-  punkteDaten.unshift(createCell("Punkte"))
-
-  const platzColumnElement = document.querySelector('.tabelle [data-type="platz"]')
-  platzColumnElement.replaceChildren(...platzDaten)
-  
-  const nameColumnElement = document.querySelector('.tabelle [data-type="name"]')
-  nameColumnElement.replaceChildren(...nameDaten)
-
-  const spieleColumnElement = document.querySelector('.tabelle [data-type="spiele"]')
-  spieleColumnElement.replaceChildren(...spieleDaten)
-
-  const toreColumnElement = document.querySelector('.tabelle [data-type="tore"]')
-  toreColumnElement.replaceChildren(...toreDaten)
-
-  const punkteColumnElement = document.querySelector('.tabelle [data-type="punkte"]')
-  punkteColumnElement.replaceChildren(...punkteDaten)
+  // Tabellenspalten aktualisieren
+  document
+    .querySelector('.tabelle [data-type="platz"]')
+    .replaceChildren(...platzDaten)
+  document
+    .querySelector('.tabelle [data-type="name"]')
+    .replaceChildren(...nameDaten)
+  document
+    .querySelector('.tabelle [data-type="spiele"]')
+    .replaceChildren(...spieleDaten)
+  document
+    .querySelector('.tabelle [data-type="tore"]')
+    .replaceChildren(...toreDaten)
+  document
+    .querySelector('.tabelle [data-type="punkte"]')
+    .replaceChildren(...punkteDaten)
 }
 
 function fillSpieltag() {
   const spieltage = saisons[aktuelleSaison].spieltage
 
-  document.querySelector('.spieltag p2 span').textContent = aktuellerSpieltag + 1 // in code we start with 0, but on the display the first day is "1"
+  // im Code ist der erste Spieltag am Index 0, aber wir zeigen auf der UI natürlich "1" an
+  document
+    .querySelector(".spieltag p2 span")
+    .textContent = aktuellerSpieltag + 1
 
+  let datumDaten
+  let heimDaten
+  let ergebnisDaten
+  let gastDaten
+  let spiele
+
+  // Daten ermitteln aus ...
   if (aktuellerSpieltag === -1) {
-    let cellElements = document.querySelectorAll('.spieltag [data-index="0"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag + 1].spiele).length; i++) {
-      cellElements[i + 1].textContent = spieltage[aktuellerSpieltag + 1].spiele[i].datum.toLocaleDateString('DE-de', { day: '2-digit', month: '2-digit', year: 'numeric'})
-    }
-
-    cellElements = document.querySelectorAll('.spieltag [data-index="1"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag + 1].spiele).length; i++) {
-      cellElements[i + 1].textContent = mannschaften[spieltage[aktuellerSpieltag + 1].spiele[i].heim].name
-    }
-
-    cellElements = document.querySelectorAll('.spieltag [data-index="2"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag + 1].spiele).length; i++) {
-      cellElements[i + 1].textContent = "tbd"
-    }
-
-    cellElements = document.querySelectorAll('.spieltag [data-index="3"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag + 1].spiele).length; i++) {
-      cellElements[i + 1].textContent = mannschaften[spieltage[aktuellerSpieltag + 1].spiele[i].gast].name
-    }
+    // ... den Spielen des ersten Spieltags, aber ohne die Ergebnisse
+    spiele = Object.values(spieltage[aktuellerSpieltag + 1].spiele)
+    datumDaten = Array.from(spiele, spiel => createCell(formatDatum(spiel.datum)))
+    heimDaten = Array.from(spiele, spiel => createCell(mannschaften[spiel.heim].name))
+    ergebnisDaten = Array.from(spiele, () => createCell('---'))
+    gastDaten = Array.from(spiele, spiel => createCell(mannschaften[spiel.gast].name))
   } else {
-    let cellElements = document.querySelectorAll('.spieltag [data-index="0"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag].spiele).length; i++) {
-      cellElements[i + 1].textContent = spieltage[aktuellerSpieltag].spiele[i].datum.toLocaleDateString('DE-de', { day: '2-digit', month: '2-digit', year: 'numeric'})
-    }
-
-    cellElements = document.querySelectorAll('.spieltag [data-index="1"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag].spiele).length; i++) {
-      cellElements[i + 1].textContent = mannschaften[spieltage[aktuellerSpieltag].spiele[i].heim].name
-    }
-
-    cellElements = document.querySelectorAll('.spieltag [data-index="2"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag].spiele).length; i++) {
-      cellElements[i + 1].textContent = spieltage[aktuellerSpieltag].spiele[i].toreHeim + ":" + spieltage[aktuellerSpieltag].spiele[i].toreGast
-    }
-
-    cellElements = document.querySelectorAll('.spieltag [data-index="3"] cell')
-    for (let i = 0; i < Object.keys(spieltage[aktuellerSpieltag].spiele).length; i++) {
-      cellElements[i + 1].textContent = mannschaften[spieltage[aktuellerSpieltag].spiele[i].gast].name
-    }
+    // ... den Spielen des gewählten Spieltags
+    spiele = Object.values(spieltage[aktuellerSpieltag].spiele)
+    ergebnisDaten = Array.from(spiele, spiel => createCell(spiel.toreHeim + ':' + spiel.toreGast))
+    datumDaten = Array.from(spiele, spiel => createCell(formatDatum(spiel.datum)))
+    heimDaten = Array.from(spiele, spiel => createCell(mannschaften[spiel.heim].name))
+    gastDaten = Array.from(spiele, spiel => createCell(mannschaften[spiel.gast].name))
   }
+
+  // Überschrift davorsetzen
+  datumDaten.unshift(createCell('Datum'))
+  heimDaten.unshift(createCell('Heimmannschaft'))
+  ergebnisDaten.unshift(createCell('Ergebnis'))
+  gastDaten.unshift(createCell('Gastmannschaft'))
+
+  // Tabellenspalten aktualisieren
+  document
+    .querySelector('.spieltag [data-type="datum"]')
+    .replaceChildren(...datumDaten)
+  document
+    .querySelector('.spieltag [data-type="heim"]')
+    .replaceChildren(...heimDaten)
+  document
+    .querySelector('.spieltag [data-type="ergebnis"]')
+    .replaceChildren(...ergebnisDaten)
+  document
+    .querySelector('.spieltag [data-type="gast"]')
+    .replaceChildren(...gastDaten)
 }
 
-// -------------------------
 function vorigerSpieltag() {
   if (aktuellerSpieltag > -1) {
     aktuellerSpieltag -= 1
     fillTabelle()
-    //fillSpieltag()
+    fillSpieltag()
   }
 }
 
@@ -446,47 +318,38 @@ function naechsterSpieltag() {
   if (aktuellerSpieltag < anzahlSpieltage - 1) {
     aktuellerSpieltag += 1
     fillTabelle()
-    //fillSpieltag()
+    fillSpieltag()
   }
 }
 
 function addEventHandler() {
-  document.getElementById('voriger-spieltag').addEventListener('click', vorigerSpieltag)
-  document.getElementById('naechster-spieltag').addEventListener('click', naechsterSpieltag)
+  document
+    .getElementById("voriger-spieltag")
+    .addEventListener("click", vorigerSpieltag)
+  document
+    .getElementById("naechster-spieltag")
+    .addEventListener("click", naechsterSpieltag)
 }
 
-
-
-
-
-
-
-
-
-
-
-console.log("Test")
-
-// siehe https://de.wikipedia.org/wiki/Spielplan_(Sport)
-
-// sei die Anzahl der Mannschaften z = 18
-// sei A die zufällige Liste der 18 Mannschaften
-// sei INDEX der Index jeder Mannschaft in der Liste
-// sei JOKER die Mannschaft mit INDEX == 18
-
-// für Spieltag n (1 <= n <= 17) gilt:
-//     Verein k spielt gegen Verein l, wobei gilt:
-//         (k + l) % 17 == 0
-// Dies wird an jedem Spieltag genau eine Mannschaft übrig lassen.
-// Diese spielt gegen den JOKER.
-
-// Ist (k + l) % 2 == 0, dann hat die Mannschaft mit dem höheren Index Heimrecht.
-// Ist (k + l) % 2 == 1, dann hat die Mannschaft mit dem niedrigeren Index Heimrecht.
-// Der Joker hat Heimrecht gegen die Mannschaften 8 - 16.
-// In der Rückrunde wird einfach getauscht.
-
-
 function erzeugeSpieltagspaarungen(spieltag) {
+  // siehe https://de.wikipedia.org/wiki/Spielplan_(Sport)
+
+  // sei die Anzahl der Mannschaften z = 18
+  // sei A die zufällige Liste der 18 Mannschaften
+  // sei INDEX der Index jeder Mannschaft in der Liste
+  // sei JOKER die Mannschaft mit INDEX == 18
+
+  // für Spieltag n (1 <= n <= 17) gilt:
+  //     Verein k spielt gegen Verein l, wobei gilt:
+  //         (k + l) % 17 == 0
+  // Dies wird an jedem Spieltag genau eine Mannschaft übrig lassen.
+  // Diese spielt gegen den JOKER.
+
+  // Ist (k + l) % 2 == 0, dann hat die Mannschaft mit dem höheren Index Heimrecht.
+  // Ist (k + l) % 2 == 1, dann hat die Mannschaft mit dem niedrigeren Index Heimrecht.
+  // Der Joker hat Heimrecht gegen die Mannschaften 8 - 16.
+  // In der Rückrunde wird einfach getauscht.
+
   const alleMannschaften = [...Array(18).keys()]
   alleMannschaften.shift()
   let mannschaft1
@@ -506,27 +369,30 @@ function erzeugeSpieltagspaarungen(spieltag) {
     }
     alleMannschaften.splice(alleMannschaften.indexOf(mannschaft2), 1)
     spiele.push({
-      heim: ((mannschaft1 + mannschaft2) % 2 === 0) ? Math.max(mannschaft1, mannschaft2) : Math.min(mannschaft1, mannschaft2),
-      gast: ((mannschaft1 + mannschaft2) % 2 === 0) ? Math.min(mannschaft1, mannschaft2) : Math.max(mannschaft1, mannschaft2)
+      heim:
+        (mannschaft1 + mannschaft2) % 2 === 0
+          ? Math.max(mannschaft1, mannschaft2)
+          : Math.min(mannschaft1, mannschaft2),
+      gast:
+        (mannschaft1 + mannschaft2) % 2 === 0
+          ? Math.min(mannschaft1, mannschaft2)
+          : Math.max(mannschaft1, mannschaft2)
     })
   }
   spiele.push({
-    heim: (alleMannschaften[0] >= 8 && alleMannschaften[0] <= 16) ? 17 : alleMannschaften[0],
-    gast: (alleMannschaften[0] >= 8 && alleMannschaften[0] <= 16) ? alleMannschaften[0] : 17
+    heim:
+      alleMannschaften[0] >= 8 && alleMannschaften[0] <= 16
+        ? 17
+        : alleMannschaften[0],
+    gast:
+      alleMannschaften[0] >= 8 && alleMannschaften[0] <= 16
+        ? alleMannschaften[0]
+        : 17
   })
 }
-
-
-
-//erzeugeSpieltagspaarungen(10)
-
-//console.log('Anzahl Spiele: %s', anzahlSpieltage)
 
 addEventHandler()
 
 const saisons = createTestspiele()
-await fillTabelle()
-//fillSpieltag()
-
-
-    
+fillTabelle()
+fillSpieltag()
